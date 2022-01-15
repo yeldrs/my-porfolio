66164dad0e6d20566a3bb23acf81845fc93056ee
yeldrs <yass.elidrissi97@gmail.com>
2022-01-15 22:23:58 +0100

    dw

--- java/java.js
+++ java/java.js
@@ -39,24 +39,3 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-//form
-    var form = document.getElementById("contact-form");
-    
-    async function handleSubmit(event) {
-      event.preventDefault();
-      var status = document.getElementById("my-form-status");
-      var data = new FormData(event.target);
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-      }).then(response => {
-        status.innerHTML = "Thanks for your submission!";
-        form.reset()
-      }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
-      });
-    }
-    form.addEventListener("submit", handleSubmit)
